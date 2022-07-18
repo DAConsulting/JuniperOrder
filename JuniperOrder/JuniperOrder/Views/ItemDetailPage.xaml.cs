85e1c7d4d4a98f880bf938b3ac4256503d7346e9
darndt <dave@davidarndt.co>
2022-07-18 14:51:32 -0400

    Add project files.

---
+++ JuniperOrder/JuniperOrder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using JuniperOrder.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace JuniperOrder.Views
+{
+   public partial class ItemDetailPage : ContentPage
+   {
+      public ItemDetailPage()
+      {
+         InitializeComponent();
+         BindingContext = new ItemDetailViewModel();
+      }
+   }
+}
---
+++ JuniperOrder/JuniperOrder/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using JuniperOrder.Models;
+using JuniperOrder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JuniperOrder.Views
+{
+   public partial class NewItemPage : ContentPage
+   {
+      public Item Item { get; set; }
+
+      public NewItemPage()
+      {
+         InitializeComponent();
+         BindingContext = new NewItemViewModel();
+      }
+   }
+}
